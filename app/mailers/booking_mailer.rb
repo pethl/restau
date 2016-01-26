2b54f4220b1772bba9351916f06128b215eb52a3
pethl <pethicklisa@gmail.com>
2016-01-26 15:50:55 +0000

    email cancellation

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -3,9 +3,16 @@ class BookingMailer < ActionMailer::Base
 
   def booking_confirmation_successful(booking)
     @booking = booking
-    #@sale = Sale.find_by(stripe_id: @charge.id)
     if @booking
       mail(to: 'contacthangfirebbq@gmail.com', subject: "Booking Confirmation for :#{@booking.name} ").deliver
     end
   end
+  
+  def booking_cancellation(booking)
+    @booking = booking
+     if @booking
+      mail(to: 'contacthangfirebbq@gmail.com', subject: "Booking Cancellation for :#{@booking.name} ").deliver
+    end
+  end
+  
 end
