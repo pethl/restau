6a4744ffabb7cd2b79e8e782ab05b18de4fbe308
pethl <pethicklisa@gmail.com>
2016-11-07 15:29:00 +0000

    early work for stats

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,5 +16,41 @@ module ApplicationHelper
   def max_diners_at_current_time(restaurant_id)
     Rdetail.where(:restaurant_id => restaurant_id)[0].max_diners_at_current_time
  end
+ 
+ def avg_days_between_booking_made_and_taken(hash_of_bookings)
+    the_days_array=[]
+     hash_of_bookings.each do |booking|
+          the_days_array << (booking.booking_date_time.to_date-booking.created_at.to_date).to_f
+         end
+   return (the_days_array.sum / the_days_array.size.to_f) 
+ end
+ 
+ def avg_days_between_booking_made_and_taken_over_6(hash_of_bookings)
+    the_days_array=[]
+     hash_of_bookings.each do |booking|
+       if booking.number_of_diners > 6
+          the_days_array << (booking.booking_date_time.to_date-booking.created_at.to_date).to_f
+        end
+         end
+         if the_days_array.size > 0
+   return (the_days_array.sum / the_days_array.size.to_f) 
+ else
+   return "N/A"
+ end
+ end
+ 
+ def avg_days_between_booking_made_and_taken_under_7(hash_of_bookings)
+    the_days_array=[]
+     hash_of_bookings.each do |booking|
+       if booking.number_of_diners < 7
+          the_days_array << (booking.booking_date_time.to_date-booking.created_at.to_date).to_f
+        end
+         end
+         if the_days_array.size > 0
+   return (the_days_array.sum / the_days_array.size.to_f) 
+ else
+   return "N/A"
+ end
+ end
 
 end
