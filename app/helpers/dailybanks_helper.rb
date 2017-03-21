c55c02f3be1d739fcb781554eea301229c6ace59
pethl <pethicklisa@gmail.com>
2017-03-21 14:45:11 +0000

    end of day fixes 8 includes hash sum fix for all reports

--- app/helpers/dailybanks_helper.rb
+++ app/helpers/dailybanks_helper.rb
@@ -4,5 +4,10 @@ module DailybanksHelper
     records.inject(0) {|sum, hash| sum + hash[field.to_sym]}
   end
   
+  #THIS WAS CREATED TO BYPASS NIL VALUES PROBLEM, occurs when past records have not been completed fully
+  def handle_nil_get_sum_from_array_field(records, field)
+    records.inject(0) {|sum, hash| sum + hash[field.to_sym].to_i}
+  end
+  
  
 end
