eb6ae865afe499d9acaa435656ad7dc2ea013762
pethl <pethicklisa@gmail.com>
2017-08-30 10:32:13 +0100

    Open bookings for Christmas 2017/18 and return to rolling 6 months bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -42,9 +42,10 @@ class Booking < ActiveRecord::Base
       #  end
       
       #13) DUP check to ensure booking is not after latest booking *currently 6 months from end of month
-       # if  (params[:booking_date]).to_date > ((Date.today.end_of_month)+6.months)
+      #CHANGE TO ALLOW XMAS BOOKINGS AND 2018 ROLLING 6 MONTHS
+        if  (params[:booking_date]).to_date > ((Date.today.end_of_month)+5.months)
        #CHANGE TO LIMIT XMAS BOOKING ON 3 JUNE 2017
-        if  (params[:booking_date]).to_date > Date.new(2017,10,31)
+       # if  (params[:booking_date]).to_date > Date.new(2017,10,31)
           return Error.get_msg("999999123")    
         end
         
@@ -172,9 +173,10 @@ class Booking < ActiveRecord::Base
 #    end
     
     #13) DUP check to ensure booking is not after latest booking *curently 6 months from end of month
-    # if  (params[:booking_date]).to_date > ((Date.today.end_of_month)+6.months)
+    #CHANGE TO ALLOW XMAS BOOKINGS AND 2018 ROLLING 6 MONTHS
+     if  (params[:booking_date]).to_date > ((Date.today.end_of_month)+5.months)
     #CHANGE TO LIMIT XMAS BOOKING ON 3 JUNE 2017
-     if  (params[:booking_date]).to_date > Date.new(2017,10,31)
+     #if  (params[:booking_date]).to_date > Date.new(2017,10,31)
         return Error.get_msg("999999123")    
       end
       
