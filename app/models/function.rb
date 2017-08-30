7fec9436835f02ff8183ec28829c252aa56f3f03
pethl <pethicklisa@gmail.com>
2017-08-30 13:12:30 +0100

    function room work, basic CRUD and index, also showing on staff calendar and in Daily bookings report

--- app/models/function.rb
+++ app/models/function.rb
@@ -8,11 +8,10 @@ class Function < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, format:  { with: VALID_EMAIL_REGEX }, :exclusion =>  { :in => %w(your@email.com), :message => " : Please enter a contact email." }
  
-  
-  DEPOSITPAID_TYPES = ["NO", "YES", "WAIVED"]
   TANDC_TYPES = ["NO", "YES"]
+  MENU_TYPES = ["NO", "YES"]
   STATUS_TYPES = ["Customer Enquiry", "Enquiry in discussion", "Confirmed"]
   EVENT_TYPES = ["Social Party", "Business Event", "Formal Dinner", "Cocktail Party", "Drinks Reception"]
   
-  
+   default_scope { order('event_date ASC') }
 end
