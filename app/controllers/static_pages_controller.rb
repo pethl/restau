9d760b51a602d64821d5313f2b123ad11b7feb3e
pethl <pethicklisa@gmail.com>
2019-03-15 16:09:54 +0000

    fixed menus download issue

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -94,7 +94,7 @@ class StaticPagesController < ApplicationController
   
   def download_menu_pdf
     send_file(
-      "#{Rails.root}/public/HF_MENU_SAMPLE_1018.pdf",
+      "#{Rails.root}/public/HF_MENU_SAMPLE_0319.pdf",
       filename: "HF_MENU_SAMPLE_0319.pdf",
       type: "application/pdf"
     )
