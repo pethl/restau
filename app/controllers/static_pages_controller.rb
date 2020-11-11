aa11fcc9efca9decd8f07ce1c1e1e41d82c11c26
Lisa Pethick <pethicklisa@gmail.com>
2020-11-11 13:52:56 +0000

    remove HaH links

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  before_action :logged_in_user, only: [:function_room_enquiry, :hfsk_pay_deposit, :hfsk_find_my_booking]
+  before_action :logged_in_user, only: [:function_room_enquiry, :hfsk_pay_deposit, :hfsk_find_my_booking, :hfsk_heat_at_home]
  
   def home
   end
